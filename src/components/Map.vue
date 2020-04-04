f5060d2ece2f990f57c7cfbdf0620063d51af86a
tuocheng <tuocheng@bytedance.com>
2020-04-04 23:32:33 +0800

    更改图标和label相对位置

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="map" class="map" id="main"></div>
+  <div ref="map" class="map" id="main" style="width:100%; height: 1000px;"></div>
 </template>
 
 <script>
@@ -17,7 +17,10 @@ export default {
       scatterSeries: [],
       effectScatterSeries: [],
       mapZoom: 6,
-      trainIcon: `path://M678.4 122.9c75.1-11.3 145.5 11.7 202.6 65.8 58 54.9 85.7 127.4 77.9 203.3-7.9 75-48.8 149-117.8 214.4-19.3 18.3-71 67.5-145.1 138.2-33.3 31.8-68.9 65.7-104.6 99.8l-34.8 33.1-13.8 13.2c-16.6 15.8-43 15.8-59.6 0L363.1 775.9l-25.4-24.2c-50.9-48.5-101.9-97-152.8-145.4C116 541 75 466.9 67.1 391.8c-7.9-76 19.9-148.3 77.9-203.3 57.1-54.1 127.6-77.1 202.6-65.8 55.8 8.5 112.3 35.4 165.4 78.5 53.2-43 109.7-70 165.4-78.3z`
+      // trainIcon: `path://M678.4 122.9c75.1-11.3 145.5 11.7 202.6 65.8 58 54.9 85.7 127.4 77.9 203.3-7.9 75-48.8 149-117.8 214.4-19.3 18.3-71 67.5-145.1 138.2-33.3 31.8-68.9 65.7-104.6 99.8l-34.8 33.1-13.8 13.2c-16.6 15.8-43 15.8-59.6 0L363.1 775.9l-25.4-24.2c-50.9-48.5-101.9-97-152.8-145.4C116 541 75 466.9 67.1 391.8c-7.9-76 19.9-148.3 77.9-203.3 57.1-54.1 127.6-77.1 202.6-65.8 55.8 8.5 112.3 35.4 165.4 78.5 53.2-43 109.7-70 165.4-78.3z`
+      // trainIcon: `path://M30.9,53.2C16.8,53.2,5.3,41.7,5.3,27.6S16.8,2,30.9,2C45,2,56.4,13.5,56.4,27.6S45,53.2,30.9,53.2z M30.9,3.5C17.6,3.5,6.8,14.4,6.8,27.6c0,13.3,10.8,24.1,24.101,24.1C44.2,51.7,55,40.9,55,27.6C54.9,14.4,44.1,3.5,30.9,3.5z M36.9,35.8c0,0.601-0.4,1-0.9,1h-1.3c-0.5,0-0.9-0.399-0.9-1V19.5c0-0.6,0.4-1,0.9-1H36c0.5,0,0.9,0.4,0.9,1V35.8z M27.8,35.8 c0,0.601-0.4,1-0.9,1h-1.3c-0.5,0-0.9-0.399-0.9-1V19.5c0-0.6,0.4-1,0.9-1H27c0.5,0,0.9,0.4,0.9,1L27.8,35.8L27.8,35.8z`
+      trainIcon: `image://https://tuocheng.oss-cn-beijing.aliyuncs.com/UTOOLS1586013674763.png`,
+      carIcon: `image://https://tuocheng.oss-cn-beijing.aliyuncs.com/UTOOLS1586015494115.png`,
     }
   },
   mounted () {
@@ -80,6 +83,14 @@ export default {
           polyline: true,
           lineStyle: { color: line.primaryColor },
           z: 3,
+          // effect:{
+          //   show:true,
+          //   period:10,
+          //   trailLength:0.3,
+          //   symbolSize:[15, 15],
+          //   color:'red',
+          //   loop:false,
+          // },
           data: [
             { // 浅色底线
               coords: line.stations.map(v => v.value),
@@ -221,20 +232,20 @@ export default {
             type: 'effectScatter', // 带有涟漪特效动画的散点（气泡）图
             coordinateSystem: 'bmap',
             symbol: this.trainIcon, // 使用自定义的SVG图标
-            symbolSize: [10, 10],
+            symbolSize: [40, 40],
             legendHoverLink: false,
             z: 6,
             effectType: 'ripple',
             rippleEffect: {
-              period: 2, // 涟漪特效的动画周期
-              scale: 5, // 涟漪特效动画中波纹的最大缩放比例
+              period: 5, // 涟漪特效的动画周期
+              scale: 1, // 涟漪特效动画中波纹的最大缩放比例
               brushType: 'fill' // 涟漪特效的波纹绘制方式
             },
             label: { // 鼠标浮动到特效点上时会显示标签
               normal: {
                 show: false,
-                position: ['500%', '300%'],
-                distance: 20,
+                position: ['150%', '150%'],
+                distance: 5,
                 color: '#222222',
                 align: 'center',
                 backgroundColor: '#FFFFFF',
